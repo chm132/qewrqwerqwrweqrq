6e3d0acceb6118e028d073e293aec6ffe4bf91e0
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2024-01-21 23:24:20 +0900

    [FEAT] 240121

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes as ReactRouters } from 'react-router-dom';
+import { Route, Routes as ReactRouters, useLocation } from 'react-router-dom';
 
 import HomePage from '../pages/Homepage/page';
 import Navbar from '../Components/Navbar';
@@ -6,12 +6,30 @@ import Navbar from '../Components/Navbar';
 import DetailPage from '../pages/CommunicationPage/DetailPage';
 import CommunityPage from '../pages/CommunicationPage/page';
 import TimeLine from '../Components/TimeLine';
+import { useEffect } from 'react';
+
+import LoginPage from '../pages/Auth/LoginPage';
+import JoinPage from '../pages/Auth/JoinPage/page';
+import SequencePage from '../pages/Auth/JoinPage/SequencePage';
 
 const Routes = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <ReactRouters>
       <Route path="/" element={<Navbar />}>
         <Route index element={<HomePage />} />
+
+        {/* 인증과정 (로그인, 회원가입) 라우팅입니다. */}
+        <Route path="auth">
+          <Route path="join" element={<JoinPage />} />
+          <Route path="join/:stage" element={<SequencePage />} />
+          <Route path="login" element={<LoginPage />} />
+        </Route>
 
         {/* 소통하러 올래 라우팅입니다 */}
         <Route
